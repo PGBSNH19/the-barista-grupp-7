affe7c9f67664821e5a2dfb66e41a0e88af9bd67
Andreas Brochs <55091646+AndreasBrochs@users.noreply.github.com>
2020-03-03 15:08:26 +0100

    Update Program.cs

--- Kaffemaskin/Kaffemaskin/Program.cs
+++ Kaffemaskin/Kaffemaskin/Program.cs
@@ -1,61 +1,87 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Kaffemaskin
 {
+    interface ICoffee
+    {
+        string MakeCoffee();
+    }
+
+    class Latte : ICoffee
+    {
+        //vatten
+        //bönor
+        //mjölk
+        public string MakeCoffee()
+        {
+            string make = "Du har gjort en latte";
+            return make;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            new Coffe("Latte")
-                    .GotMilk()
-                    .Roast()
-                    .Size()
-                .MakeCoffe();
+
+
+            //new Coffee("Latte")
+            //        .GotMilk()
+            //        .Roast()
+            //        .Size()
+            //    .MakeCoffe();
         }
     }
-    class Coffe : ICoffe 
-    {
-        private ICoffe<CoffePart> CoffeParts {get; set; }
-        private string Name {get; set; }
+    //class Coffee : ICoffee 
+    //{
+    //    public ICoffee GotMilk();
+    //    private IEnumerable<CoffeePart> CoffeParts {get; set; }
+    //    private string Name {get; set; }
     
-        public Coffe (string name)
-        {
-            CoffeParts = new List<CoffePart>();
-            Name = name;
-        }
+    //    public Coffee (string name)
+    //    {
+    //        CoffeParts = new List<CoffeePart>();
+    //        Name = name;
+    //    }
 
-    }
+    //}
 
-    interface ICoffee
-    {
-        ICoffee GotMilk();
-        ICoffee Size(int number);
-        ICoffee AddWater(int number);
+    //interface ICoffee
+    //{
+    //    ICoffee GotMilk();
+    //    ICoffee Size(int number);
+    //    ICoffee AddWater(int number);
 
-    }
+    //}
+
+    //class CoffeePart
+    //{
+
+    //}
 
 
-    class Beans
-    {
-        string[] roast = { "ljus", "mellan", "mörk" };
-        string[] country = { "Colombia", "Central America", "Arabica" };
-        bool eko;
-        string[] manufacturer = {"Gevalia", "Lövbergs Lila", "Starbucks" };
+    //class Beans
+    //{
+    //    string[] roast = { "ljus", "mellan", "mörk" };
+    //    string[] country = { "Colombia", "Central America", "Arabica" };
+    //    bool eko;
+    //    string[] manufacturer = {"Gevalia", "Lövbergs Lila", "Starbucks" };
 
-    }
-    class Machine
-    {
-        string sort;
-        string bean;
-        int temp;
-        int[] water = {2,4,6};
-        bool milk;
+    //}
+    //class Machine
+    //{
+    //    string sort;
+    //    string bean;
+    //    int temp;
+    //    int[] water = {2,4,6};
+    //    bool milk;
         
-    }
-    class Cup
-    {
-        string[] size = { "small", "medium", "large" };
-        string[] sort = { "latte", "espresso", "black", "cappucino" };
+    //}
+    //class Cup
+    //{
+    //    string[] size = { "small", "medium", "large" };
+    //    string[] sort = { "latte", "espresso", "black", "cappucino" };
 
-    }
+    //}
 }
