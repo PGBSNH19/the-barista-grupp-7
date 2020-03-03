28d58e0269476acce6533926883e3e563044e8ff
Andreas Brochs <55091646+AndreasBrochs@users.noreply.github.com>
2020-03-03 13:19:00 +0100

    @AndreasBrochs

---
+++ Kaffemaskin/Kaffemaskin/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Kaffemaskin
+{
+    class Beans
+    {
+
+    }
+    class Machine
+    {
+
+    }
+    class Cup
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
