38bcd852229733919b4d5ada6754ae2e1a88a9ac
Andreas Brochs <55091646+AndreasBrochs@users.noreply.github.com>
2020-03-03 13:28:47 +0100

    @AndreasBrochs

--- Kaffemaskin/Kaffemaskin/Program.cs
+++ Kaffemaskin/Kaffemaskin/Program.cs
@@ -4,11 +4,19 @@ namespace Kaffemaskin
 {
     class Beans
     {
+        string sort;
+        string country;
+        bool eko;
+        string manufacturer;
 
     }
     class Machine
     {
-
+        string sort;
+        string bean;
+        int temp;
+        int[] water = {2,4,6 };
+        
     }
     class Cup
     {
